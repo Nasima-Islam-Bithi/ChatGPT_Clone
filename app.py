3dbc67eb5b6e390c98efbf4472c04022bb382e07
Nasima-Islam-Bithi <69648480+Nasima-Islam-Bithi@users.noreply.github.com>
2025-09-19 17:07:58 +0200

    Update app.py

--- app.py
+++ app.py
@@ -51,4 +51,5 @@ with block:
     submit = gr.Button("SEND")
     submit.click(chatgpt_clone, inputs=[message, state], outputs=[chatbot, state])
 
-block.launch(debug = True)
+
+block.launch(auth=('user', 'pass'), auth_message='Enter your user credential')
